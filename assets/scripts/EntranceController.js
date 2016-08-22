44d6f096a674daa1582698427faf127350d8d09b
Trung Nghiem Toan <trungnt85@gmail.com>
2016-08-22 11:50:52 +0700

    initial commit

---
+++ assets/scripts/EntranceController.js
@@ -0,0 +1,26 @@
+
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+      registerButton: {
+            default: null,
+            type: cc.Node
+      },
+      backButton: {
+            default: null,
+            type: cc.Node
+      }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
---
+++ assets/scripts/RegisterController.js
@@ -0,0 +1,25 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+      accountNameLabel: {
+          default: null,
+          type: cc.Label
+      },
+      accountNameInput: {
+          default: null,
+          type: cc.EditBox
+      }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+      this.accountNameLabel.string = "AAA";
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
