c344e1aa192e55cd6b915e8133b1d8e52bdd9a07
Trung Nghiem Toan <trungnt85@gmail.com>
2016-08-28 11:21:20 +0700

    done displaying game list icons base on sfs config

--- assets/scripts/components/scenes/DashboardScene.js
+++ assets/scripts/components/scenes/DashboardScene.js
@@ -1,6 +1,6 @@
 var game = require('game');
-
 var item = require('item');
+
 cc.Class({
     extends: cc.Component,
 
@@ -34,49 +34,50 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        var sendObject = {
-            'cmd' : 'gv',
-            'data' : {
-                'pid' : 1
-            }
-        };
-        console.log('request list game');
-        game.service.send(sendObject, (data) => {
-            console.log(data);
+            var sendObject = {
+                'cmd' : 'gv',
+                'data' : {
+                    'pid' : 1
+                }
+            };
+            console.log('request list game');
+            game.service.send(sendObject, (data) => {
+                console.log(data);
 
 
-        this.gameList = data["cl"];
-
+            this.gameList = data["cl"];
             console.log(this.gameList);
 
 
 
-            this._initItemListGame();
-        },game.const.scene.DASHBOARD_SCENE);
+                this._initItemListGame();
+            }
+        ,game.const.scene.DASHBOARD_SCENE);
 
     },
 
 
     _initItemListGame:function () {
 
-        let height = this.scrollerContentView.node.height;
-        let itemDimension = height / 2.0 - 80;
+        var height = this.scrollerContentView.node.height;
+        var itemDimension = height / 2.0 - 80;
 
         for (var i = 0; i <  this.gameList.length ; i++) {
 
 
-            let nodeItem = new cc.instantiate(this.item);
-            // console.log(nodeItem);
-           let sprite = nodeItem.getComponent(cc.Sprite);
-            nodeItem.setContentSize(itemDimension,itemDimension);
-
-            this.scrollerContentView.node.addChild(nodeItem);
-            nodeItem.getComponent(item).addOnClickListener((gameCode) => {
-                    console.log("click Item");
-            });
-
+            // nodeItem.getComponent(item).addOnClickListener((gameCode) => {
+            //         console.log('click Item' + gameCode);
+            // });
+            var itemPrefab = this.item;
+            var container = this.scrollerContentView;
             cc.loader.loadRes(game.resource.gameIcon[this.gameList[i]], cc.SpriteFrame, function (err, spriteFrame) {
+
+                var nodeItem = new cc.instantiate(itemPrefab);
+                nodeItem.setContentSize(itemDimension,itemDimension);
+                container.node.addChild(nodeItem);
+
                 var sprite = nodeItem.getComponent(cc.Sprite);
+
                 sprite.spriteFrame = spriteFrame ;
             });
         }
