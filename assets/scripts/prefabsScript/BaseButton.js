bf38857b57cac54c96f27bb1e51dcb67d447ea47
Khoa Le <lengockhoa90@gmail.com>
2016-09-01 11:52:36 +0700

    add base button prefab

--- assets/scripts/prefabsScript/BaseButton.js
+++ assets/scripts/prefabsScript/BaseButton.js
@@ -28,13 +28,39 @@ cc.Class({
         // },
         // ...
 
-    buttonType: game.bottomBarButtonType.NAPXU
+        buttonType: game.bottomBarButtonType.NAPXU,
+        pressedScale: 1,
+        transDuration: 0
     },
 
     // use this for initialization
     onLoad: function () {
 
+
+        this._addAnimationWhenTouch();
+
     },
+
+    // add animation ưhen touch
+    _addAnimationWhenTouch: function() {
+        var self = this;
+        self.initScale = this.node.scale;
+        self.button = self.getComponent(cc.Button);
+        self.scaleDownAction = cc.scaleTo(self.transDuration, self.pressedScale);
+        self.scaleUpAction = cc.scaleTo(self.transDuration, self.initScale);
+        function onTouchDown (event) {
+            this.stopAllActions();
+            this.runAction(self.scaleDownAction);
+        }
+        function onTouchUp (event) {
+            this.stopAllActions();
+            this.runAction(self.scaleUpAction);
+        }
+        this.node.on('touchstart', onTouchDown, this.node);
+        this.node.on('touchend', onTouchUp, this.node);
+        this.node.on('touchcancel', onTouchUp, this.node);
+
+    }
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
