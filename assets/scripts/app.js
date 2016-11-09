1638f3905593d2f68d23c59ffa1dfe29dd9aa033
meodien99 <seb.madcat@gmail.com>
2016-11-09 10:21:15 +0700

    commit Object.assign if platform is native

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -174,6 +174,30 @@ if (cc.sys.isBrowser) {
 } else {
     window.Promise = Promise;
 
+    if (typeof Object.assign != 'function') {
+        (function() {
+            Object.assign = function(target) {
+                'use strict';
+                // We must check against these specific cases.
+                if (target === undefined || target === null) {
+                    throw new TypeError('Cannot convert undefined or null to object');
+                }
+
+                var output = Object(target);
+                for (var index = 1; index < arguments.length; index++) {
+                    var source = arguments[index];
+                    if (source !== undefined && source !== null) {
+                        for (var nextKey in source) {
+                            if (source.hasOwnProperty(nextKey)) {
+                                output[nextKey] = source[nextKey];
+                            }
+                        }
+                    }
+                }
+                return output;
+            };
+        })();
+    }
     if (cc.sys.IOS) {
         app.DEVICE_ID = jsb.reflection.callStaticMethod("FCUUID", "uuidForDevice");
         log(`ios udid ${app.DEVICE_ID}`);
