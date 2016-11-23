c2726a94fd2f86fb2247511b5503693e4f33ff5c
Trung Nghiem Toan <trungnt85@gmail.com>
2016-11-23 15:53:50 +0700

    add dao cu

---
+++ assets/scripts/components/prefabs/popup/FriendProfilePopup.js
@@ -0,0 +1,77 @@
+import app from 'app';
+import Component from 'Component';
+import  NodeRub from 'NodeRub';
+
+export default class FriendProfilePopup extends Component {
+    constructor() {
+        super();
+
+        this.propsGridView = {
+            default: null,
+            type: cc.Layout,
+        };
+
+    }
+    propsItemClicked(e){
+
+        const clipName = e.target.name;
+
+        const animation = this.node.getChildByName('animatedNode').getComponent(cc.Animation);
+        // const animation = this.getComponent(cc.Animation);
+
+        cc.loader.loadRes(`props/${clipName}`, cc.SpriteAtlas, (err, atlas) => {
+            var spriteFrames = atlas.getSpriteFrames();
+
+            var clip = cc.AnimationClip.createWithSpriteFrames(spriteFrames, 10);
+            clip.name = 'run';
+            clip.wrapMode = cc.WrapMode.Default;
+
+            animation.addClip(clip);
+            animation.play('run');
+        });
+
+    }
+
+    loadPropsAssets(){
+        cc.loader.loadResAll('props/thumbs',cc.SpriteFrame, function (err, assets) {
+            if (err) {
+                cc.error(err);
+                return;
+            }
+
+            let gridViewHeigh = this.propsGridView.node.height;
+            gridViewHeigh = (gridViewHeigh) / 2.0;
+
+            assets.forEach((asset, index) => {
+                // console.debug(`${index} `, asset);
+
+                const node = new cc.Node();
+                node.width = gridViewHeigh;
+                node.height = gridViewHeigh;
+
+                const spr = node.addComponent(cc.Sprite);
+                spr.spriteFrame  = asset;
+
+                const button = node.addComponent(cc.Button);
+
+                const clickEvent = new cc.Component.EventHandler();
+                clickEvent.target = this.node;
+                clickEvent.component = 'FriendProfilePopup';
+                clickEvent.handler = 'propsItemClicked';
+
+                node.name = asset.name;
+                button.clickEvents = [clickEvent];
+
+                this.propsGridView.node.addChild(node);
+
+            });
+        }.bind(this));
+    }
+
+
+    onLoad(){
+        this.loadPropsAssets();
+    }
+}
+
+app.createComponent(FriendProfilePopup);
