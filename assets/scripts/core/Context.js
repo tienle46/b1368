23c2523321128cbb0b6af9b3b9a2a22a00cca3ee
madcat <nam.hd@gzone.com.vn>
2017-05-30 14:16:52 +0700

    fix conflict

--- assets/scripts/core/Context.js
+++ assets/scripts/core/Context.js
@@ -88,7 +88,7 @@ class GameContext {
         let username = this.getMyInfo() ? this.getMyInfo().name : null;
         if (username)
             return this.purchaseItems.filter(item => item.username = username) || [];
-        return [];
+        return this.purchaseItems || [];
     }
 
     /**
