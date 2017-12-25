0745587a4a4e6f678ece1d035fa4eab7a50b295c
Macbook <macbook@Macbooks-Mac-mini.local>
2017-12-25 16:05:16 +0700

    add caothap TopPopup

---
+++ assets/scripts/components/prefabs/dialog/body/HighLow/HighLowTopItem.js
@@ -0,0 +1,65 @@
+import app from 'app';
+import Actor from 'Actor';
+import GameUtils from 'GameUtils';
+import Utils from 'GeneralUtils';
+
+class HighLowTopItem extends Actor {
+    constructor() {
+        super();
+
+        this.properties = this.assignProperties({
+            lblRank: cc.Label,
+            lblTime: cc.Label,
+            lblUsername: cc.Label,
+            lblBet: cc.Label,
+            lblInfo: cc.Label,
+            lblAmount: cc.Label,
+
+        });
+
+        this.topRankColor1 = new cc.Color(255, 242, 0, 255);
+        this.topRankColor2 = new cc.Color(235, 235, 235, 255);
+        this.MAX_USER_LENGTH = 8;
+    }
+
+    loadData(idx, data) {
+        if (idx <= 2) {
+            this.lblRank.node.color = this.topRankColor1;
+        } else {
+            this.lblRank.node.color = this.topRankColor2;
+        }
+
+        var rank = idx + 1;
+        var time = this.formatDate(data.time);
+        var username = this.formatUsername(data.playerName);
+        var bet = data.bet || 0;
+        var info = data.info || '';
+        var win = data.winAmount || 0;
+        // var cards = data.cards;
+
+        this.lblRank.string = rank;
+        this.lblTime.string = '' + time;
+        this.lblUsername.string = username;
+        this.lblBet.string = GameUtils.formatNumberType1(bet);
+        this.lblInfo.string = info;
+        this.lblWin.string = GameUtils.formatNumberType1(win);
+
+        // this.cardHand.loadCardsByValues(cards);
+    }
+
+    formatUsername(username) {
+        var result = username;
+        if (username.length > this.MAX_USER_LENGTH) {
+            result = username.slice(0, this.MAX_USER_LENGTH) + '...';
+        }
+
+        return result;
+    }
+
+    formatDate(dateString) {
+         return Utils.timeFormat(dateString, 'DD-MM-YYYY HH:mm:ss', 'DD-MM-YY HH:mm');
+    }
+
+}
+
+app.createComponent(HighLowTopItem);
---
+++ assets/scripts/components/prefabs/dialog/body/HighLow/HighLowTopPopup.js
@@ -0,0 +1,68 @@
+import app from 'app';
+import BasePopup from 'BasePopup';
+import HighLowTopItem from 'HighLowTopItem';
+import CCUtils from 'CCUtils';
+
+class HighLowTopPopup extends BasePopup {
+    constructor() {
+        super();
+
+        this.properties = this.assignProperties({
+            container: cc.Node,
+            itemPrefab: cc.Node
+        });
+
+    }
+
+    onEnable() {
+        super.onEnable();
+
+        // this._registerEventListener();
+
+        // this._sendGetTop();
+    }
+
+    onDisable() {
+        super.onDisable();
+
+        // this._removeItems();
+        // this._deregisterEventListener();
+    }
+
+    _sendGetTop() {
+        app.service.send({cmd:app.commands.MINIGAME_MINI_POKER_TOP_PLAYER});
+    }
+
+    _registerEventListener() {
+        app.system.addListener(app.commands.MINIGAME_MINI_POKER_TOP_PLAYER, this._onReceivedTop, this);
+    }
+
+    _deregisterEventListener() {
+        app.system.removeListener(app.commands.MINIGAME_MINI_POKER_TOP_PLAYER, this._onReceivedTop, this);
+    }
+
+    _onReceivedTop(data) {
+        // warn('top', data);
+        this._removeItems();
+
+        data.topPlayers.forEach((info, idx) => {
+            this._addItem(idx, info);
+        });
+
+    }
+
+    _removeItems() {
+        CCUtils.clearAllChildren(this.container);
+    }
+
+    _addItem(idx, data) {
+        var item = cc.instantiate(this.itemPrefab);
+        item.active = true;
+        var itemCtrl = item.getComponent(HighLowTopItem);
+        itemCtrl.loadData(idx, data);
+        this.container.addChild(item);
+    }
+
+}
+
+app.createComponent(HighLowTopPopup);
