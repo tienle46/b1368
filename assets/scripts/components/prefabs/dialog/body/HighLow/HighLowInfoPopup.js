a6791fe571e7936860bf8ff78a9bafcf893fa9c0
Trinh7293 <trinh7293@gmail.com>
2018-01-22 10:41:18 +0700

    fix popup info highlow

--- assets/scripts/components/prefabs/dialog/body/HighLow/HighLowInfoPopup.js
+++ assets/scripts/components/prefabs/dialog/body/HighLow/HighLowInfoPopup.js
@@ -46,15 +46,19 @@ class HighLowInfoPopup extends BasePopup {
     _generateRichText() {
         let content = 
         `Cách chơi:`
-        + `<br/><color=#efbb01>- Bước 1</c>: Chọn mức cược bạn muốn tham gia chơi. Có các mức cược mặc định là: `
-        + `1000, 10000, 50000, 100000, 500000.`
-        + `<br/><color=#efbb01>- Bước 2</c>: Bấm “Chơi ngay” để bắt đầu chơi. Hệ thống sẽ trả về ngẫu nhiên 1 lá bài để bạn đưa ra dự đoán của mình về lá bài tiếp theo`
+        + `<br/><color=#efbb01>- Bước 1</c>: Chọn mức cược bạn muốn tham gia chơi. Có các mức cược mặc định`
+        + `   là: 1000, 10000, 50000, 100000, 500000.`
+        + `<br/><color=#efbb01>- Bước 2</c>: Bấm “Chơi ngay” để bắt đầu chơi. Hệ thống sẽ trả về ngẫu nhiên 1 lá`
+        + `<br/>   bài để bạn đưa ra dự đoán của mình về lá bài tiếp theo`
         + `<br/><color=#efbb01>- Bước 3</c>: Thực hiện dự đoán lá bài tiếp theo lớn hoặc nhỏ hơn lá bài hiện tại.`
-        + `Bạn có 2p để đưa ra dự đoán của mình. Sau 2p mà chưa đưa ra dự đoán thì hệ thống sẽ tự động đưa ra 1 lựa chọn ngẫu nhiên`
+        + `<br/>   Bạn có 2p để đưa ra dự đoán của mình. Sau 2p mà chưa đưa ra dự đoán thì hệ`
+        + `   thống sẽ tự động đưa ra 1 lựa chọn ngẫu nhiên`
         + `<br/><color=#efbb01>- Bước 4</c>: Theo dõi kết quả để biết Thắng – Thua.`
         + `<br/><color=#FE4C45>Đặc biệt </c>`
-        + `<br/>- Nếu lá bài tiếp theo trùng với lá bài hiện tại thì hệ thống sẽ trích <color=#efbb01>10%</c> tiền cược của người chơi cho vào Hũ thưởng`
-        + `<br/>- <color=#FE4C45>NỔ HŨ</c>: Khi bạn thu thập đủ 3 cây Át (A) trong cùng 1 phiên chơi sẽ được <color=#FE4C45>NỔ HŨ</c> và dành được <color=#efbb01>50%</c> giá trị HŨ`
+        + `<br/>- Nếu lá bài tiếp theo trùng với lá bài hiện tại thì hệ thống sẽ trích <color=#efbb01>10%</c> tiền cược`
+        + `<br/>   của người chơi cho vào Hũ thưởng`
+        + `<br/>- <color=#FE4C45>NỔ HŨ</c>: Khi bạn thu thập đủ 3 cây Át (A) trong cùng 1 phiên chơi sẽ được NỔ`
+        + `<br/>   HŨ và dành được <color=#efbb01>50%</c> giá trị HŨ THƯỞNG`
         
         this.richTextInfo.string = content
     }
