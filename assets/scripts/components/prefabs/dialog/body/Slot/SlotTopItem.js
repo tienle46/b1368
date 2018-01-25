8612e57a75b8ed81e5f7fbf60ea36ea11d30da86
tienle46 <tienle4695@gmail.com>
2018-01-25 15:59:45 +0700

    fix Items Popup + add listener

--- assets/scripts/components/prefabs/dialog/body/Slot/SlotTopItem.js
+++ assets/scripts/components/prefabs/dialog/body/Slot/SlotTopItem.js
@@ -8,28 +8,44 @@ class SlotTopItem extends Actor {
         super();
 
         this.properties = this.assignProperties({
-            LblTime: cc.Label,
+            LblRank: cc.Label,
             lblUsername: cc.Label,
             lblBet: cc.Label,
             lblWin: cc.Label,
-            lblDetail: cc.Label
-
+            lblDetail: cc.Label,
+            sprRank: cc.Node,
+            spriteFrame1st : cc.SpriteFrame,
+            spriteFrame2nd: cc.SpriteFrame,
+            spriteFrame3rd: cc.SpriteFrame
 
         });
 
     }
 
     loadData(data) {
-        console.log('dataItem', data)
-        this.LblTime.string = '' + this.formatDate(data.time)
+        let rank = data.rank
+        if (rank < 4) {
+
+            switch (rank) {
+                case 1:
+                    this.sprRank.getComponent(cc.Sprite).spriteFrame = this.spriteFrame1st
+                    break
+                case 2:
+                    this.sprRank.getComponent(cc.Sprite).spriteFrame = this.spriteFrame2nd
+                    break
+                case 3:
+                    this.sprRank.getComponent(cc.Sprite).spriteFrame = this.spriteFrame3rd
+                    break
+            }
+
+        } else {
+            this.LblRank.string = data.rank
+        }
+
         this.lblUsername.string = data.username
         this.lblBet.string = GameUtils.formatNumberType1(data.bet)
         this.lblWin.string = GameUtils.formatNumberType1(data.win)
         this.lblDetail.string = data.detail
-    }
-
-    formatDate(dateString) {
-        return Utils.timeFormat(dateString, 'DD-MM-YYYY HH:mm:ss', 'DD-MM-YY HH:mm');
     }
 
 }
--- assets/scripts/components/prefabs/dialog/body/Slot/SlotTopPopUp.js
+++ assets/scripts/components/prefabs/dialog/body/Slot/SlotTopPopUp.js
@@ -1,6 +1,6 @@
 import BasePopup from 'BasePopup'
 import SlotTopItem from 'SlotTopItem';
-
+import CCUtils from 'CCUtils';
 
 class SlotTopPopup extends BasePopup {
     constructor() {
@@ -14,32 +14,62 @@ class SlotTopPopup extends BasePopup {
     }
     onEnable() {
         super.onEnable();
-        this.scheduleOnce(this.initValue, 0.2);
+        this._registerEventListener()
+        this.scheduleOnce(this._sendGetTop, 0.2);
     }
 
     onDisable() {
         super.onDisable();
+        this._deregisterEventListener()
 
     }
-    initValue() {
-        const data = []
-        const currentTime = new Date()
+
+
+    _sendGetTop() {
+        const sendObj = { cmd: app.commands.MINIGAME_CAO_THAP_TOP_PLAYER }
+        warn('sendObj', sendObj)
+        // app.service.send(sendObj);
+        var data = []
         for(let i=0;i<15;i++) {
             data.push({
-                time: currentTime,
+                rank: i+1,
                 username: `aneo${i}`,
                 bet: 1000+i,
                 win: 2000*i,
                 detail: 'Nổ hũ'
             })
         }
-        console.log(data)
-        data.forEach(i=>{
-            let item = cc.instantiate(this.itemPrefab)
-            item.active = true
-            item.getComponent(SlotTopItem).loadData(i)
-            this.container.addChild(item)
-        })
+        this._onReceivedTop(data)
+    }
+
+    _registerEventListener() {
+        app.system.addListener(app.commands.MINIGAME_CAO_THAP_TOP_PLAYER, this._onReceivedTop, this);
+    }
+
+    _deregisterEventListener() {
+        app.system.removeListener(app.commands.MINIGAME_CAO_THAP_TOP_PLAYER, this._onReceivedTop, this);
+    }
+
+    _onReceivedTop(data) {
+        warn('top', data);
+        this._removeItems();
+
+        data.forEach((info, idx) => {
+            this._addItem(idx, info);
+        });
+
+    }
+
+    _removeItems() {
+        CCUtils.clearAllChildren(this.container);
+    }
+
+
+    _addItem(idx, info) {
+        let item = cc.instantiate(this.itemPrefab)
+        item.active = true
+        item.getComponent(SlotTopItem).loadData(info)
+        this.container.addChild(item)
     }
 }
 
